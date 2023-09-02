e2c1b6492fa9d141ca520323b40e99e467d254b4
Hippie Hacker <hh@ii.coop>
2023-09-02 07:06:53 +0100

    Install tunnel from a github release
    Rather than compiling it

--- iimatey-setup.sh
+++ iimatey-setup.sh
@@ -2,55 +2,44 @@
 set -x
 set -e
 
+# What Architecture
+case $(uname -m) in
+  arm64)
+    export ARCH=arm64
+    ;;
+  amd64)
+    export ARCH=amd64
+    ;;
+  x86_64)
+    export ARCH=amd64
+    ;;
+esac
+
 case $(uname -o) in
   GNU/Linux)
     sudo apt-get update
     sudo apt-get install -y ttyd tmux wireguard-tools curl
     export OS=linux
-  ;;
+    ;;
   Darwin)
     brew install ttyd tmux wireguard-tools curl
     export OS=darwin
-  ;;
-esac
-# What Architecture
-case $(uname -m) in
-  arm64)
-    export ARCH=arm64
-  ;;
-  amd64)
-    export ARCH=amd64
-  ;;
-  x86_64)
-    export ARCH=amd64
-  ;;
+    ;;
 esac
 
-# ## TODO: publish tunnel client binaries
-# Othewise we need to build tunnel... we will download go into /tmp, compile, and delete go
-# WARNING WE WILL OVER WRITE THE VERSION OF GO in /usr/local/go
-GO_VERSION=1.20.2
-
-export GO_TMP_DIR=$(mktemp -d)
-# Ensure tunnel binary exists
-curl -sSL https://dl.google.com/go/go${GO_VERSION}.${OS}-${ARCH}.tar.gz \
-    | gunzip -d \
-    | tar --directory $GO_TMP_DIR --extract
-
-# GO
-export PATH=$GO_TMP_DIR/go/bin:$PATH
-go install github.com/coder/wgtunnel/cmd/tunnel@v0.1.5
-# Move generated tunnel go binary to /usr/local/bin
-# Not sure where we should put it, for now to /usr/local/bin/tunnel
-sudo mv $HOME/go/bin/tunnel /usr/local/bin/tunnel
-rm -rf $GO_TMP_DIR
+# Install tunnel from github release
+TUNNEL_RELEASE=v0.1.13
+TUNNEL_URL=https://github.com/cloudnative-coop/wgtunnel/releases/download/$TUNNEL_RELEASE/tunnel-$OS-$ARCH
+sudo curl -L -o /usr/local/bin/tunnel $TUNNEL_URL
+sudo chmod 0755 /usr/local/bin/tunnel # make executeable
 
 # WHAT WE NEED
 tmux -V
 ttyd --version
 wg version
 /usr/local/bin/tunnel --version
-echo $PATH | grep /usr/local/bin > /dev/null || echo "You may want to add /usr/local/bin to your PATH"
+echo $PATH | grep /usr/local/bin >/dev/null || echo "You may want to add /usr/local/bin to your PATH"
 
+# Install iimatey script from github
 sudo curl -o /usr/local/bin/iimatey -L https://raw.githubusercontent.com/ii/matey/canon/iimatey
 sudo chmod +x /usr/local/bin/iimatey
