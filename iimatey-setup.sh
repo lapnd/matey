eff7ec5dfa32bcc3b0dd1ad488628775f4efc914
lapnd <lapd@viosoft.com>
2024-07-30 19:53:57 +0700

    Update iimatey-setup.sh

--- iimatey-setup.sh
+++ iimatey-setup.sh
@@ -2,43 +2,69 @@
 set -x
 set -e
 
-# What Architecture
+# Determine the architecture
 case $(uname -m) in
   arm64)
     export ARCH=arm64
     ;;
-  amd64)
-    export ARCH=amd64
-    ;;
-  x86_64)
+  amd64|x86_64)
     export ARCH=amd64
     ;;
 esac
 
-case $(uname -o) in
-  GNU/Linux)
-    sudo apt-get update
-    sudo apt-get install -y ttyd tmux curl
+# Determine the operating system
+case $(uname -s) in
+  Linux)
     export OS=linux
+    # Further check for distribution
+    if [[ -f /etc/redhat-release ]]; then
+      # CentOS/RHEL
+      sudo yum update -y || sudo dnf update -y
+      sudo yum install -y jq tmux curl || sudo dnf install -y jq tmux curl
+    elif [[ -f /etc/debian_version ]]; then
+      # Debian/Ubuntu
+      sudo apt-get update
+      sudo apt-get install -y jq tmux curl
+    else
+      echo "Unsupported Linux distribution"
+      exit 1
+    fi
+
+    # Get the latest ttyd release version
+    LATEST_TTYD=$(curl -s https://api.github.com/repos/tsl0922/ttyd/releases/latest | jq -r .tag_name)
+    TTYD_URL="https://github.com/tsl0922/ttyd/releases/download/$LATEST_TTYD/ttyd.x86_64"
+
+    # Download and install ttyd
+    sudo curl -L -o /usr/local/bin/ttyd $TTYD_URL
+    sudo chmod 0755 /usr/local/bin/ttyd
+
+    # Check ttyd version
+    ttyd --version
     ;;
   Darwin)
     brew install ttyd tmux curl
     export OS=darwin
     ;;
+  *)
+    echo "Unsupported OS"
+    exit 1
+    ;;
 esac
 
-# Install tunnel from github release
+# Install tunnel from GitHub release
 TUNNEL_RELEASE=v0.1.14
 TUNNEL_URL=https://github.com/ii/wgtunnel/releases/download/$TUNNEL_RELEASE/tunnel-$OS-$ARCH
 sudo curl -L -o /usr/local/bin/tunnel $TUNNEL_URL
-sudo chmod 0755 /usr/local/bin/tunnel # make executeable
+sudo chmod 0755 /usr/local/bin/tunnel
 
-# WHAT WE NEED
+# Check versions
 tmux -V
 ttyd --version
 /usr/local/bin/tunnel --version
+
+# Add /usr/local/bin to PATH if not already present
 echo $PATH | grep /usr/local/bin >/dev/null || echo "You may want to add /usr/local/bin to your PATH"
 
-# Install iimatey script from github
-sudo curl -o /usr/local/bin/iimatey -L https://raw.githubusercontent.com/ii/matey/canon/iimatey
+# Install iimatey script from GitHub
+sudo curl -L -o /usr/local/bin/iimatey https://raw.githubusercontent.com/ii/matey/canon/iimatey
 sudo chmod +x /usr/local/bin/iimatey
